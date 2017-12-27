975b1a5d485d9ac7dd977867add90c60b3391c51
ubbn <ubbn@live.com>
2017-12-27 19:17:25 +0100

    MaterialUi for CommentForm

--- frontend/src/Components/CommentForm.js
+++ frontend/src/Components/CommentForm.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-material-ui'
+import { FlatButton } from 'material-ui'
 
-const CommentForm = (props) => {
+const required = value => value ? undefined : 'Required'
+
+const CommentForm = props => {
   const { handleSubmit, onCancel, pristine, submitting } = props
+  const isNew = !props.initialValues
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} style={isNew ? {margin: '30px'} : {}}>
+      {isNew && 'Add a comment'}
       <div>
-        <Field name="author" type="text" placeholder="Author" component="input"/>
+        <Field name="author" type="text" hintText="Author" component={TextField}/>
       </div>
       <div>
-        <Field name="body" type="text" placeholder="Comment" component="textarea"/>
+        <Field name="body" type="text" hintText="Comment" component={TextField} validate={required}/>
       </div>
       <div>
-        <button type="submit" disabled={pristine || submitting}>Save</button>
-        <button type="button" onClick={onCancel}>Cancel</button>
+        <FlatButton type="submit" disabled={pristine || submitting} label={isNew ? "Add" : "Save"} />
+        <FlatButton type="button" onClick={onCancel} label="Cancel" />
       </div>      
     </form>
   );
